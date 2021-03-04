d3c814268481d714983ccbea0f88ff1e89439d7c
nekacurry <anneka498@gmail.com>
2021-03-04 16:03:46 -0600

    finished secret message

--- app.py
+++ app.py
@@ -67,12 +67,21 @@ def favorites_results():
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
+    return """
+      <form action="/message_results" method="POST">
+          What is your secret message? <br/>
+          <input type="text" name="message"><br/>
+          <input type="submit" value="Submit!">
+      </form>
+
+      """
 
 @app.route('/message_results', methods=['POST'])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
-    pass
+    user_message = request.form.get('message')
+
+    return f'Here is your secret message!: {sort_letters(user_message)}'
 
 @app.route('/calculator')
 def calculator():
