e0ec3286a82f37b930f8ff30a0e5920d107f15a7
nekacurry <anneka498@gmail.com>
2021-03-04 16:28:58 -0600

    refactored froyo and calculator templates

--- app.py
+++ app.py
@@ -17,24 +17,21 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    return """
-      <form action="/froyo_results" method="GET">
-          What is your favorite Fro-Yo flavor? <br/>
-          <input type="text" name="flavor"><br/>
-          What topping would you like? <br/>
-          <input type="text" name="toppings"><br/>
-          <input type="submit" value="Submit!">
-      </form>
 
-      """
+    return render_template('froyo_form.html')
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    users_froyo_flavor = request.args.get('flavor')
-    users_froyo_toppings = request.args.get('toppings')
+    
+    
+    context = {
+      'users_froyo_flavor' : request.args.get('flavor'),
+      'users_froyo_toppings' : request.args.get('toppings')
+    }
 
-    return f'You ordered {users_froyo_flavor} flavored Fro-Yo with toppings {users_froyo_toppings}!'
+
+    return render_template('froyo_results.html', **context)
 
 @app.route('/favorites')
 def favorites():
@@ -86,20 +83,8 @@ def message_results():
 @app.route('/calculator')
 def calculator():
     """Shows the user a form to enter 2 numbers and an operation."""
-    return """
-    <form action="/calculator_results" method="GET">
-        Please enter 2 numbers and select an operator.<br/><br/>
-        <input type="number" name="operand1">
-        <select name="operation">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" name="operand2">
-        <input type="submit" value="Submit!">
-    </form>
-    """
+
+    return render_template('calculator_form.html')
 
 @app.route('/calculator_results')
 def calculator_results():
